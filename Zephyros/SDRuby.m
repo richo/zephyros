a3900d9777833b3316f7e5e2c602f6217f1add92
Steven Degutis <steven@cleancoders.com>
2013-07-29 10:19:47 -0500

    half of rubycocoa

--- Zephyros/SDRuby.m
+++ Zephyros/SDRuby.m
@@ -12,37 +12,148 @@
 #include <ruby/ruby.h>
 
 
+@interface SDRubyBlock : NSObject
+@property VALUE inner;
++ (SDRubyBlock*) withRubyValue:(VALUE)val;
+@end
+
+@implementation SDRubyBlock
+
+- (id) call:(NSArray*)args {
+    return nil;
+}
+
++ (SDRubyBlock*) withRubyValue:(VALUE)val {
+    SDRubyBlock* block = [[SDRubyBlock alloc] init];
+    block.inner = val;
+    return block;
+}
+
+@end
+
+
+
+
+VALUE SDObjcToRubyValue(id obj) {
+    return Qnil;
+}
+
+
+id SDRubyToObjcValue(VALUE obj) {
+    switch (TYPE(obj)) {
+        case T_FIXNUM:
+            return @(FIX2LONG(obj));
+        case T_FLOAT:
+            return @(NUM2DBL(obj));
+        case T_STRING:
+            return [NSString stringWithUTF8String:StringValueCStr(obj)];
+            break;
+        case T_SYMBOL:
+            return [NSString stringWithUTF8String:rb_id2name(SYM2ID(obj))];
+            break;
+        case T_TRUE:
+            return @YES;
+        case T_FALSE:
+            return @NO;
+        case T_NIL:
+            return [NSNull null];
+        case T_HASH:
+        {
+            VALUE array_of_kv_tuples = rb_funcall(obj, rb_intern("to_a"), 0);
+            
+            NSMutableDictionary* dict = [NSMutableDictionary dictionary];
+            VALUE* ptr = RARRAY_PTR(array_of_kv_tuples);
+            
+            for (int i = 0; i < RARRAY_LEN(array_of_kv_tuples); i++) {
+                VALUE kv_tuple = ptr[i];
+                VALUE* tuple_ptr = RARRAY_PTR(kv_tuple);
+                
+                [dict setObject:SDRubyToObjcValue(tuple_ptr[1])
+                         forKey:SDRubyToObjcValue(tuple_ptr[0])];
+            }
+            
+            return dict;
+        }
+        case T_ARRAY:
+        {
+            NSMutableArray* array = [NSMutableArray array];
+            VALUE* ptr = RARRAY_PTR(obj);
+            
+            for (int i = 0; i < RARRAY_LEN(obj); i++) {
+                VALUE v = ptr[i];
+                id o = SDRubyToObjcValue(v);
+                [array addObject:o];
+            }
+            
+            return array;
+        }
+        case T_DATA:
+            return [SDRubyBlock withRubyValue:obj];
+        default:
+            rb_raise(rb_eTypeError, [[NSString stringWithFormat:@"not valid value, class = %@", SDRubyToObjcValue(rb_funcall(CLASS_OF(obj), rb_intern("name"), 0))] UTF8String]);
+            break;
+    }
+    
+    return nil;
+}
+
 
 
 VALUE sd_method_missing(VALUE self, VALUE args) {
+    BOOL has_block = NO;
+    VALUE given_block;
+    if (rb_block_given_p()) {
+        given_block = rb_block_proc();
+        has_block = YES;
+    }
+    
+    
+//    NSLog(@"%@", SDRubyToObjcValue(rb_funcall(CLASS_OF(args), rb_intern("name"), 0)));
+//    return Qnil;
+    
     void* s;
     Data_Get_Struct(self, void, s);
-    id real = (__bridge_transfer id)s;
+    id internalObj = (__bridge_transfer id)s;
     
-    NSLog(@"im called! [%@]", real);
-    return Qnil;
+    NSMutableArray* objsArgs = SDRubyToObjcValue(args);
+    
+    if (has_block)
+        [objsArgs addObject:SDRubyToObjcValue(given_block)];
+    
+//    NSLog(@"%@", objsArgs);
+    
+    NSString* selStr = [objsArgs objectAtIndex:0];
+    SEL sel = NSSelectorFromString(selStr);
+    
+    [objsArgs removeObjectAtIndex:0];
+    
+    NSInvocation* inv = [[NSInvocation alloc] init];
+    
+    [inv setSelector:sel];
+    [inv setTarget:internalObj];
+    
+    int i = 0;
+    for (id arg in objsArgs) {
+        __unsafe_unretained id tempArg = arg;
+        [inv setArgument:&tempArg atIndex:i++];
+    }
+    
+    [inv invoke];
+    
+    id result;
+    [inv getReturnValue:&result];
+    
+    return SDObjcToRubyValue(result);
 }
 
 
 
 VALUE sd_bind_keys_fn(VALUE module, VALUE key, VALUE mods) {
-    VALUE p = rb_block_proc();
-    
-    
-    
-    rb_funcall(p, rb_intern("call"), 0);
-    
-    NSString* ss = [NSString stringWithUTF8String:StringValueCStr(key)];
-    NSLog(@"it was [%@]", ss);
-
-//    double delayInSeconds = 2.0;
-//    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-//    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//        rb_funcall(callback, rb_intern("call"), 0);
-//    });
+//    VALUE p = rb_block_proc();
+//    rb_funcall(p, rb_intern("call"), 0);
 //    
-//
-    NSLog(@"lolcat");
+//    NSLog(@"key = [%@]", SDRubyToObjcValue(key));
+//    NSLog(@"mods = [%@]", SDRubyToObjcValue(mods));
     return Qnil;
 }
 
@@ -80,8 +191,9 @@ VALUE SDWrappedObject(char* klass, id thing) {
     VALUE wrapped = SDWrappedObject("Window", thing);
     rb_iv_set(rb_eval_string("self"), "@something", wrapped);
     
-    rb_eval_string("puts @something.class");
-    rb_eval_string("@something.woo");
+    
+    
+    rb_eval_string("@something.woo(true, false, lambda{ puts 'whoa' }, nil, :hi, {:a => 2, :b => [:hi]}) { puts 'hi' }");
 }
 
 - (void) evalString:(NSString*)code {
