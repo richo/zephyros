f11b37c83eb8ff6c60dd3132ba113e3dc70466d3
Steven Degutis <steven@cleancoders.com>
2013-08-01 02:19:03 -0500

    fixed

--- Zephyros/SDClient.m
+++ Zephyros/SDClient.m
@@ -11,36 +11,25 @@
 @implementation SDClient
 
 - (void) waitForNewMessage {
-    
-    [self.sock readDataToLength:2 withTimeout:0 tag:0];
-    
-//    [self.sock readDataToData:[@"\n" dataUsingEncoding:NSUTF8StringEncoding]
-//                  withTimeout:3
-//                          tag:0];
+    [self.sock readDataToData:[@"\n" dataUsingEncoding:NSUTF8StringEncoding]
+                  withTimeout:3
+                          tag:0];
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag {
-    NSLog(@"here..");
-    
-    NSString* str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    [self handleMessage:str];
-    
-    [self waitForNewMessage];
-    
-//    if (tag == 0) {
-//        NSString* str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//        NSInteger size = [str integerValue];
-//        
-//        [self.sock readDataToLength:size
-//                        withTimeout:3
-//                                tag:1];
-//    }
-//    else if (tag == 1) {
-////        id obj = [NSJSONSerialization JSONObjectWithData:data options:0 error:0];
-//        id obj = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//        [self handleMessage:obj];
-//        [self waitForNewMessage];
-//    }
+    if (tag == 0) {
+        NSString* str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSInteger size = [str integerValue];
+        
+        [self.sock readDataToLength:size
+                        withTimeout:3
+                                tag:1];
+    }
+    else if (tag == 1) {
+        id obj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:0];
+        [self handleMessage:obj];
+        [self waitForNewMessage];
+    }
 }
 
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err {
@@ -49,15 +38,21 @@
 
 - (void) handleMessage:(id)msg {
     NSLog(@"new msg: %@", msg);
+    
+    if ([[msg objectAtIndex:1
+          ] isEqual:@3])
+        [self sendMessage:[@[@"foobar", @12] mutableCopy]];
+    
     [self sendMessage:msg];
 }
 
 - (void) sendMessage:(id)msg {
-//    NSData* data = [NSJSONSerialization dataWithJSONObject:msg options:NSJSONWritingPrettyPrinted error:NULL];
-    NSData* data = [msg dataUsingEncoding:NSUTF8StringEncoding];
-//    NSString* len = [NSString stringWithFormat:@"%ld", [data length]];
-//    [self.sock writeData:[len dataUsingEncoding:NSUTF8StringEncoding] withTimeout:3 tag:0];
-//    [self.sock writeData:[GCDAsyncSocket LFData] withTimeout:3 tag:0];
+    [msg replaceObjectAtIndex:0 withObject:@"response"];
+    
+    NSData* data = [NSJSONSerialization dataWithJSONObject:msg options:0 error:NULL];
+    NSString* len = [NSString stringWithFormat:@"%ld", [data length]];
+    [self.sock writeData:[len dataUsingEncoding:NSUTF8StringEncoding] withTimeout:3 tag:0];
+    [self.sock writeData:[GCDAsyncSocket LFData] withTimeout:3 tag:0];
     [self.sock writeData:data withTimeout:3 tag:0];
 }
 
--- hi.rb
+++ hi.rb
@@ -1,97 +1,149 @@
 require 'socket'
 require 'json'
+require 'thread'
 
-class Zeph
+@s = TCPSocket.new 'localhost', 1235
 
-  def initialize
-    @s = TCPSocket.new 'localhost', 1235
-    at_exit { wait_for_responses }
+@id = 0
 
-    @request_id = 0
+def send(*data)
+  @id += 1
+  json = ['request', @id].concat(data).to_json
+  s = "#{json.size}\n#{json}"
+  @s.write s
 
-    @thread = Thread.new { wait_for_msg }
-    @responses = []
-  end
-
-  def send_msg(data)
-    @request_id += 1
-    data = ['request', @request_id, data]
-    json = data.to_json
-    # @s.write "#{json.size}\n#{json}"
-    @s.write 'hi'
-    @s.flush
-
-    puts 'sent'
-
-    loop do
-      if @responses.size > 0
-        return @responses.pop
-      end
-      # founds = @responses.select{|response| response[1] == @request_id}
-      # if founds.size > 0
-      #   puts 'FOUND!'
-      #   @responses.delete_if{|response| response[1] == @request_id}
-      #   p @responses
-      #   return founds.first
-      # end
-    end
-  end
-
-  def handle_msg(msg)
-    puts 'response:'
-    p msg
-    @responses << msg
-  end
-
-  private
-
-  def wait_for_msg
-    loop do
-      # size = @s.gets
-      puts 'waiting...'
-      data = @s.readpartial(2)
-      puts 'GOT...'
-      str, pi = data
-      handle_msg str
-    end
-  end
-
-  def wait_for_responses
-    @thread.join
-    @s.close
-  end
-
+  get_message(@id)
 end
 
-$z = Zeph.new
-
-
-
-
-class Window
-  def size
-    $z.send_msg(['sup'])
-  end
+def get
+  size = @s.gets
+  msg = @s.readpartial(size.to_i)
+  JSON.load(msg)
 end
 
-w = Window.new
+@queue = Queue.new
+
+@thread = Thread.new do
+  loop { @queue << get }
+end
+
+@responses = []
+
+def get_message(id)
+  loop do
+    r = @queue.pop
+    if r[1] == id
+      return r
+    else
+      @responses << r
+    end
+  end
+end
 
 10.times do
-  puts "the thing is #{w.size}"
+  val = send 'set_title', 'woot'
+  p val
 end
 
-puts 'ok then'
+p @responses
+
+@thread.join
+
+@s.close
+
+
+
+# class Zeph
+
+#   def initialize
+#     @s = TCPSocket.new 'localhost', 1235
+#     at_exit { wait_for_responses }
+
+#     @request_id = 0
+
+#     @thread = Thread.new { wait_for_msg }
+#     @responses = []
+#   end
+
+#   def send_msg(data)
+#     @request_id += 1
+#     data = ['request', @request_id, data]
+#     json = data.to_json
+#     @s.write "#{json.size}\n#{json}"
+#     # @s.write 'hi'
+#     @s.flush
+
+#     puts 'sent'
+
+#     loop do
+#       # if @responses.size > 0
+#       #   return @responses.pop
+#       # end
+#       founds = @responses.select{|response| response[1] == @request_id}
+#       if founds.size > 0
+#         puts 'FOUND!'
+#         @responses.delete_if{|response| response[1] == @request_id}
+#         p @responses
+#         return founds.first
+#       end
+#     end
+#   end
+
+#   def handle_msg(msg)
+#     puts 'response:'
+#     p msg
+#     @responses << msg
+#   end
+
+#   private
+
+#   def wait_for_msg
+#     loop do
+#       size = @s.gets
+#       puts 'waiting...'
+#       data = @s.readpartial(size.to_i)
+#       puts 'GOT...'
+#       str, pi = data
+#       handle_msg str
+#     end
+#   end
+
+#   def wait_for_responses
+#     @thread.join
+#     @s.close
+#   end
+
+# end
+
+# $z = Zeph.new
 
 
 
 
-__END__
+# class Window
+#   def size
+#     $z.send_msg(['sup'])
+#   end
+# end
 
-[:request,
- 3,
- {
-   :receiver => 3,
-   :method => "size",
-   :args => ["size"],
- }
-]
+# w = Window.new
+
+# 10.times do
+#   puts "the thing is #{w.size}"
+# end
+
+# puts 'ok then'
+
+
+
+
+# __END__
+
+# [:request,
+#  3,
+#  {
+#    :receiver => 3,
+#    :method => "size",
+#    :args => ["size"],
+#  }
+# ]
