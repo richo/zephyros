cca3657eb9b9f5819a9181e814c67913a42e5085
Steven Degutis <steven@cleancoders.com>
2013-08-03 11:46:59 -0500

    meh

--- libs/zeph-test.go
+++ libs/zeph-test.go
@@ -16,19 +16,14 @@ func main() {
 		f.X += 10
 		win.SetTopLeft(f)
 
-
 		API.ChooseFrom([]string{"foo", "bar"}, "title", 20, 20, func(i int) {
 			fmt.Println(i)
 		})
-
-		// send(API, "choose_from", []string{"foo", "bar"}, "title", 20, 20, func(i interface{}) {
-		// 	fmt.Println("inner!", i)
-		// })
 	})
 
-	// API.Listen("app_launched", func(app App) {
-	// 	API.Alert(app.Title(), 1)
-	// })
+	API.Listen("app_launched", func(app App) {
+		API.Alert(app.Title(), 1)
+	})
 
 	ListenForCallbacks()
 }
