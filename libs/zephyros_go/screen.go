dc73134eed539cb8626e7a1597953a55b9ec4a92
Steven Degutis <steven@cleancoders.com>
2013-08-03 11:39:11 -0500

    screend one

--- libs/zephyros_go/screen.go
+++ libs/zephyros_go/screen.go
@@ -5,3 +5,31 @@ import (
 )
 
 type Screen float64
+
+func (self Screen) FrameIncludingDockAndMenu() Rect {
+	var buf Rect
+	bytes := send(float64(self), nil, false, "frame_including_dock_and_menu")
+	json.Unmarshal(bytes, &buf)
+	return buf
+}
+
+func (self Screen) FrameWithoutDockOrMenu() Rect {
+	var buf Rect
+	bytes := send(float64(self), nil, false, "frame_without_dock_or_menu")
+	json.Unmarshal(bytes, &buf)
+	return buf
+}
+
+func (self Screen) NextScreen() Screen {
+	var buf Screen
+	bytes := send(float64(self), nil, false, "next_screen")
+	json.Unmarshal(bytes, &buf)
+	return buf
+}
+
+func (self Screen) PreviousScreen() Screen {
+	var buf Screen
+	bytes := send(float64(self), nil, false, "previous_screen")
+	json.Unmarshal(bytes, &buf)
+	return buf
+}
