a48af60d6b5a266ecd5eb85b32fe9ab08c6f8d45
Steven Degutis <steven@cleancoders.com>
2013-08-10 15:15:32 -0500

    [#52] Make log window always on top.

--- Zephyros/SDLogWindowController.m
+++ Zephyros/SDLogWindowController.m
@@ -94,11 +94,13 @@
     NSLog(@"key down! %@", theEvent);
 }
 
-- (void) windowDidBecomeKey:(NSNotification *)notification {
-    self.window.level = NSNormalWindowLevel;
-}
+//- (void) windowDidBecomeKey:(NSNotification *)notification {
+//    self.window.level = NSNormalWindowLevel;
+//}
 
 - (void) windowDidLoad {
+    self.window.level = NSFloatingWindowLevel;
+    
     self.replHistory = [NSMutableArray array];
     self.webView.frameLoadDelegate = self;
     
@@ -118,7 +120,6 @@
 
 - (void) show:(NSString*)message type:(NSString*)type {
     if (!self.window.isVisible) {
-        self.window.level = NSFloatingWindowLevel;
         [self showWindow:nil];
     }
     
