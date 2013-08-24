8dde8af5bdd97b9a683d9be99ea8b8be6ce8c072
Steven Degutis <steven@cleancoders.com>
2013-08-24 15:22:01 -0500

    Fixes #84

--- Zephyros/SDShellCommand.m
+++ Zephyros/SDShellCommand.m
@@ -13,21 +13,24 @@
 @property NSTask* task;
 @property NSTask* taskLifeWatcher;
 
+@property NSPipe* stdoutPipe;
+@property NSPipe* stderrPipe;
+
 @end
 
 @implementation SDShellCommand
 
 - (void) launch {
-    NSPipe* stdoutPipe = [NSPipe pipe];
-    NSPipe* stderrPipe = [NSPipe pipe];
+    self.stdoutPipe = [NSPipe pipe];
+    self.stderrPipe = [NSPipe pipe];
     
     self.task = [[NSTask alloc] init];
     
     [self.task setLaunchPath:@"/bin/bash"];
     [self.task setArguments:@[@"-l", @"-c", self.cmd]];
     
-    [self.task setStandardOutput:stdoutPipe];
-    [self.task setStandardError:stderrPipe];
+    [self.task setStandardOutput:self.stdoutPipe];
+    [self.task setStandardError:self.stderrPipe];
     
     self.taskLifeWatcher =
     [[NSNotificationCenter defaultCenter] addObserverForName:NSTaskDidTerminateNotification
@@ -38,15 +41,18 @@
                                                   }];
     
     if (self.gotStdout)
-        [stdoutPipe fileHandleForReading].readabilityHandler = self.gotStdout;
+        [self.stdoutPipe fileHandleForReading].readabilityHandler = self.gotStdout;
     
     if (self.gotStderr)
-        [stderrPipe fileHandleForReading].readabilityHandler = self.gotStderr;
+        [self.stderrPipe fileHandleForReading].readabilityHandler = self.gotStderr;
     
     [self.task launch];
 }
 
 - (void) kill {
+//    NSLog(@"killed");
+    [self closePipes];
+    
     pid_t p = [self.task processIdentifier];
     [self.task terminate];
     kill(p, SIGKILL);
@@ -57,7 +63,18 @@
     return [self.task isRunning];
 }
 
+- (void) closePipes {
+    [self.stdoutPipe.fileHandleForReading closeFile];
+    [self.stderrPipe.fileHandleForReading closeFile];
+    
+    self.stdoutPipe = nil;
+    self.stderrPipe = nil;
+}
+
 - (void) taskDied {
+//    NSLog(@"died");
+    [self closePipes];
+    
     [[NSNotificationCenter defaultCenter] removeObserver:self.taskLifeWatcher];
     
     if (self.died)
