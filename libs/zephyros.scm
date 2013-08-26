99a808d412f790c3ff310090ab70aa766c765a29
Richo Healey <richo@psych0tik.net>
2013-08-26 15:15:36 -0700

    Add stub for chicken scheme implementation

---
+++ libs/zephyros.scm
@@ -0,0 +1,39 @@
+; Requires the medea egg
+; $ chicken-install medea
+
+(use tcp)
+(use medea)
+
+(define *zephyros-host*
+  (let ((host (get-environment-variable "ZEPHYROS_HOST")))
+    (or host "localhost")))
+
+(define *zephyros-port*
+  (let ((port (get-environment-variable "ZEPHYROS_PORT")))
+    (if port
+      (string->number port)
+      1235)))
+
+(define-values (zeph-in zeph-out)
+  (tcp-connect *zephyros-host* *zephyros-port*))
+
+(define call/next-id
+  (let ((value 0))
+    (lambda (proc)
+      (set! value (add1 value))
+      (proc value))))
+
+(define (send datum)
+  (call/next-id (lambda (id)
+    (with-output-to-port zeph-out (lambda ()
+      (let* ((payload (apply vector id 'null datum))
+             (json-payload (json->string payload))
+             (json-length (string-length json-payload)))
+        (write-string (number->string json-length))
+        (write-string "\n")
+        (write-string json-payload)))))))
+
+;; Begin userfacing API
+
+(define (alert message duration)
+  (send (list "alert" message duration)))
